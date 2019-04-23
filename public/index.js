7f713a100718bfc4de3e630fc9a8614d84c752db
sabeel29 <sabeel1999.29@gmail.com>
2019-04-23 15:06:55 +0300

    adding new code to deal with the input

---
+++ public/index.js
@@ -0,0 +1,9 @@
+const searchValue = document.getElementById("search").value;
+document.getElementById("button").addEventListener("click", getSearchValue);
+
+const getSearchValue = (event) => {
+  event.preventDefault();
+  // fetch(searchValue)
+}
+
+// fetch()
